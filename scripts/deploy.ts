64cbe8639917b4dcd3673b3560f6faea5a97818a
CodeWarrier <CodeWarrier3000@gmail.com>
2022-03-03 16:01:09 +0900

    updated

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,16 +4,22 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
+import { utils } from 'ethers'
 
 const personalSignPrefix = "\x19Ethereum Signed Message:\n";
 
 async function main() {
   const [owner, addr1] = await ethers.getSigners();
 
-  const ERC721A = await ethers.getContractFactory("ERC721AMock");
-  const erc721A = await ERC721A.deploy("Trip", "TRIP");
-  await erc721A.deployed();
-  console.log("erc721A:", erc721A.address);
+  // const ERC721A = await ethers.getContractFactory("ERC721AMock");
+  // const erc721A = await ERC721A.deploy("Trip", "TRIP");
+  // await erc721A.deployed();
+  // console.log("erc721A:", erc721A.address);
+  const ERC20 = await ethers.getContractFactory("TestERC20");
+  const erc20 = await ERC20.deploy();
+  await erc20.deployed();
+  console.log("erc20: ", erc20.address);
+  await erc20.mint(owner.address, utils.parseEther("1000000"));
 
   // const WyvernAtomicizer = await ethers.getContractFactory("WyvernAtomicizer");
   // const wyvernAtomicizer = await WyvernAtomicizer.deploy();
